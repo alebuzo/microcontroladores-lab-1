e7e625e4244d6a0003a145245f00fc09b5c82838
alebuzo <al.cbuzo@gmail.com>
2023-01-13 16:07:14 -0600

    Borrar funciones no utilizadas

--- lab1.c
+++ lab1.c
@@ -1,35 +1,13 @@
 #include <pic14/pic12f675.h>
 #include <stdint.h>
 #include <stdio.h>
-//int next = 250;
+
 
 typedef unsigned int word;
-word __at 0x2007 __CONFIG = (_MCLRE_OFF&_WDTE_OFF);
+word __at 0x2007 __CONFIG = (_WDTE_OFF);
 
  
-//To compile:
-//sdcc -mpic14 -p16f675 blink.c
- 
-//To program the chip using picp:
-//Assuming /dev/ttyUSB0 is the serial port.
- 
-//Erase the chip:
-//picp /dev/ttyUSB0 16f887 -ef
- 
-//Write the program:
-//picp /dev/ttyUSB0 16f887 -wp blink.hex
- 
-//Write the configuration words (optional):
-//picp /dev/ttyUSB0 16f887 -wc 0x2ff4 0x3fff
- 
-//Doing it all at once: erasing, programming, and reading back config words:
-//picp /dev/ttyUSB0 16f887 -ef -wp blink.hex -rc
- 
-//To program the chip using pk2cmd:
-//pk2cmd -M -PPIC16f887 -Fblink.hex
- 
 void delay (unsigned inttiempo);
-//int rand ();
 uint8_t rotl(const uint8_t x, int k);
 uint8_t next(void);
 int rand(void);
@@ -70,11 +48,7 @@ void main(void)
                 case 3:
                 { 
                     GPIO = 0b00010001;
-                    //GP4 = 1;
-                    //GP0 = 1;
 			        delay(time);
-			        //GP4 = 0;
-                    //GP0 = 0;
                     GPIO = 0b00000000;
                     break;
                 }
@@ -82,11 +56,7 @@ void main(void)
                 case 4:
                 { 
                     GPIO = 0b00010010;
-                    //GP4 = 1;
-                    //GP1 = 1;
 			        delay(time);
-			        //GP4 = 0;
-                    //GP1 = 0;
                     GPIO = 0b00000000;
                     break;
                 }
@@ -94,13 +64,7 @@ void main(void)
                 case 5:
                 {
                     GPIO = 0b00010011;
-                    //GP4 = 1;
-                    //GP1 = 1;
-                    //GP0 = 1;
 			        delay(time);
-			        //GP4 = 0;
-                    //GP1 = 0;
-                    //GP0 = 0;
                     GPIO = 0b00000000;
                     break;
                 }
@@ -108,13 +72,7 @@ void main(void)
                 case 6:
                 { 
                     GPIO = 0b00010110;
-                    //GP4 = 1;
-                    //GP1 = 1;
-                    //GP2 = 1;
 			        delay(time);
-			        //GP4 = 0;
-                    //GP1 = 0;
-                    //GP2 = 0;
                     GPIO = 0b00000000;
                     break;
                 }
@@ -129,9 +87,6 @@ void main(void)
 }
 
 
- 
-
-
 void delay(unsigned int tiempo)
 {
 	unsigned int i;
@@ -141,19 +96,6 @@ void delay(unsigned int tiempo)
 	  for(j=0;j<1275;j++);
 }
 
-/*int rand ()
-{
-    
-    while (1) { 
-        next = next * 11035245 + 1235; 
-        int randi =  (next/6535) % 7;
-        if (randi < 7 && randi > 0) {
-            return randi;
-        }
-        
-    }
-    //return (unsigned int) (next/65535) % 32768;
-}*/
 
 uint8_t rotl(const uint8_t x, int k) {
     return (x << k) | (x >> (8 - k));
